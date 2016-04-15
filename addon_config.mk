2e163d84172f5c71a28c209776b8a63dbfe87339
Christopher Baker <me@christopherbaker.net>
2016-04-15 11:44:16 -0500

    Add addon_config.mk.

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+meta:
+	ADDON_NAME = ofxTime
+	ADDON_DESCRIPTION = A collection of utilities for doing Date / Time math.
+	ADDON_AUTHOR = bakercp
+	ADDON_TAGS = "date" "time" "datetime" "date math" "math"
+	ADDON_URL = http://github.com/bakercp/ofxTime
